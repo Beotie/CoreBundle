6518641891b4b6bedfd6404f5faf379b664b24a9
matthieu88160 <matthieu.vallance@cscfa.fr>
2017-08-31 12:26:23 +0200

    Resolve unused code

--- CoreBundle/DependencyInjection/Configuration.php
+++ CoreBundle/DependencyInjection/Configuration.php
@@ -18,12 +18,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('beotie_core');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
+        $treeBuilder->root('beotie_core');
+        
         return $treeBuilder;
     }
 }
