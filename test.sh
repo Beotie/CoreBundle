1de3ad60ba4ca26792829e4fa1c1e8b177677b61
matthieu88160 <matthieu.vallance@cscfa.fr>
2017-09-16 13:24:17 +0200

    Improve test.sh capability

--- test.sh
+++ test.sh
@@ -1,9 +1,37 @@
 #! /usr/bin/env bash
 
 function usage {
-  echo "Usage: $0 <PHP/PATH>" 1>&2
+  echo "Usage: $0 [-p path/to/php] [-c path/to/composer] [-h]" 1>&2
   exit 1
 }
+
+PHP_PATH="/usr/bin/env php"
+COMPOSER_PATH="/usr/bin/env composer"
+
+while getopts ":p:c:h" opt; do
+  case $opt in
+    p)
+      PHP_PATH=$OPTARG
+      ;;
+    c)
+      COMPOSER_PATH=$OPTARG
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
 
 STATUS=0
 TEST_RES=""
@@ -34,25 +62,25 @@ then
     mkdir -p "doc"
 fi
 
-runner "/usr/bin/env php vendor/bin/phpcbf --standard=./csruleset.xml CoreBundle/"
+runner "$PHP_PATH vendor/bin/phpcbf --standard=./csruleset.xml CoreBundle/"
 echo "$TEST_RES" >> doc/phpcbf.txt
 
-test "/usr/bin/env php vendor/bin/phpunit" PHPUnit 100
+test "$PHP_PATH vendor/bin/phpunit" PHPUnit 100
 echo "$TEST_RES" > doc/phpunit.txt
 
-test "/usr/bin/env php vendor/bin/phpcs --standard=./csruleset.xml CoreBundle/" PHPCS 100
+test "$PHP_PATH vendor/bin/phpcs --standard=./csruleset.xml CoreBundle/" PHPCS 100
 echo "$TEST_RES" > doc/phpcs.txt
 
-test "/composer/composer.phar validate" COMPOSER 100
+test "$COMPOSER_PATH validate" COMPOSER 100
 echo "$TEST_RES" > doc/composer.txt
 
-test "/usr/bin/env php vendor/bin/phpmd CoreBundle/ text ./phpmd.xml" PHPMD 100
+test "$PHP_PATH vendor/bin/phpmd CoreBundle/ text ./phpmd.xml" PHPMD 100
 echo "$TEST_RES" > doc/phpmd.txt
 
-test "/usr/bin/env php vendor/bin/phpcpd CoreBundle/" PHPCPD 1
+test "$PHP_PATH vendor/bin/phpcpd CoreBundle/" PHPCPD 1
 echo "$TEST_RES" > doc/phpcpd.txt
 
-test "/usr/bin/env php vendor/bin/phpmetrics --report-html=doc/metrics CoreBundle/" PHPMETRICS 1
+test "$PHP_PATH vendor/bin/phpmetrics --report-html=doc/metrics CoreBundle/" PHPMETRICS 1
 echo "$TEST_RES" > doc/phpmetrics.txt
 
 if [ "$STATUS" -eq 0 ]
