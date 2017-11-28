e30986c49ed49122eed6d53d3ad26303a00375b1
matthieu88160 <matthieu.vallance@cscfa.fr>
2017-11-28 12:24:36 +0100

    Add StringUri scheme and constructor test case

---
+++ CoreBundle/Tests/Request/Uri/StringUri/StringUriSchemePart.php
@@ -0,0 +1,158 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is part of beotie/core_bundle
+ *
+ * As each files provides by the CSCFA, this file is licensed
+ * under the MIT license.
+ *
+ * PHP version 7.1
+ *
+ * @category Test
+ * @package  Beotie_Core_Bundle
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+namespace Beotie\CoreBundle\Tests\Request\Uri\StringUri;
+
+use Beotie\CoreBundle\Request\Uri\PortMappingInterface;
+use Beotie\CoreBundle\Request\Uri\StringUri;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * StringUri scheme part
+ *
+ * This trait is used to validate the StringUri instance relative to scheme management
+ *
+ * @category Test
+ * @package  Beotie_Core_Bundle
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+trait StringUriSchemePart
+{
+    /**
+     * Scheme provider
+     *
+     * This method provide a set of scheme to validate the StringUri methods
+     *
+     * @return [[string]]
+     */
+    public function schemeProvider()
+    {
+        $schemes = array_keys(PortMappingInterface::MAPPING);
+
+        return array_map([$this, 'toArray'], $schemes);
+    }
+
+    /**
+     * Test withScheme
+     *
+     * This method validate the StringUri::withScheme method
+     *
+     * @param string $scheme The scheme to use as new scheme
+     *
+     * @dataProvider schemeProvider
+     * @return       void
+     * @covers       Beotie\CoreBundle\Request\Uri\StringUri::withScheme
+     */
+    public function testWithScheme(string $scheme) : void
+    {
+        $currentScheme = 'http';
+
+        $reflex = new \ReflectionClass(StringUri::class);
+        $instance = $reflex->newInstanceWithoutConstructor();
+
+        $propertyReflex = $reflex->getProperty('scheme');
+        $propertyReflex->setAccessible(true);
+
+        $propertyReflex->setValue($instance, $currentScheme);
+
+        $newInstance = $instance->withScheme($scheme);
+
+        $this->getTestCase()->assertEquals($currentScheme, $propertyReflex->getValue($instance));
+        $this->getTestCase()->assertEquals($scheme, $propertyReflex->getValue($newInstance));
+
+        return;
+    }
+
+    /**
+     * Test getScheme
+     *
+     * This method validate the StringUri::getScheme method
+     *
+     * @param string $scheme The scheme to use as new scheme
+     *
+     * @dataProvider schemeProvider
+     * @return       void
+     * @covers       Beotie\CoreBundle\Request\Uri\StringUri::getScheme
+     */
+    public function testGetScheme(string $scheme) : void
+    {
+        $reflex = new \ReflectionClass(StringUri::class);
+        $instance = $reflex->newInstanceWithoutConstructor();
+
+        $propertyReflex = $reflex->getProperty('scheme');
+        $propertyReflex->setAccessible(true);
+        $propertyReflex->setValue($instance, $scheme);
+
+        $this->getTestCase()->assertEquals($scheme, $instance->getScheme());
+
+        return;
+    }
+
+    /**
+     * Test getSchemeElement
+     *
+     * This method validate the StringUri::getSchemeElement method
+     *
+     * @param string $scheme The scheme to use as scheme
+     *
+     * @dataProvider schemeProvider
+     * @return       void
+     * @covers       Beotie\CoreBundle\Request\Uri\StringUri::getSchemeElement
+     */
+    public function testGetSchemeElement(string $scheme) : void
+    {
+        $reflex = new \ReflectionClass(StringUri::class);
+        $instance = $reflex->newInstanceWithoutConstructor();
+
+        $reflexMethod = $reflex->getMethod('getSchemeElement');
+        $reflexMethod->setAccessible(true);
+
+        $this->getTestCase()->assertEmpty($reflexMethod->invoke($instance));
+
+        $propertyReflex = $reflex->getProperty('scheme');
+        $propertyReflex->setAccessible(true);
+        $propertyReflex->setValue($instance, $scheme);
+
+        $this->getTestCase()->assertEquals(sprintf('%s:', $scheme), $reflexMethod->invoke($instance));
+
+        return;
+    }
+
+    /**
+     * To array
+     *
+     * This method insert a value into an empty array
+     *
+     * @param mixed $value The value to inject
+     *
+     * @return array
+     */
+    protected function toArray($value) : array
+    {
+        return [$value];
+    }
+
+    /**
+     * Get test case
+     *
+     * This method return the current test case to be used as assertion provider
+     *
+     * @return TestCase
+     */
+    protected abstract function getTestCase() : TestCase;
+}
---
+++ CoreBundle/Tests/Request/Uri/StringUriTest.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is part of beotie/core_bundle
+ *
+ * As each files provides by the CSCFA, this file is licensed
+ * under the MIT license.
+ *
+ * PHP version 7.1
+ *
+ * @category Test
+ * @package  Beotie_Core_Bundle
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+namespace Beotie\CoreBundle\Tests\Request\Uri;
+
+use PHPUnit\Framework\TestCase;
+use Beotie\CoreBundle\Request\Uri\StringUri;
+use function False\true;
+use Beotie\CoreBundle\Tests\Request\Uri\StringUri\StringUriSchemePart;
+
+/**
+ * String uri test
+ *
+ * This class is used to validate the StringUri instance
+ *
+ * @category Test
+ * @package  Beotie_Core_Bundle
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+class StringUriTest extends TestCase
+{
+    use StringUriSchemePart;
+
+    /**
+     * Uri provider
+     *
+     * This method provide a set of data to validate the StringUri methods
+     *
+     * @return [[string, [string|int]]
+     */
+    public function uriProvider()
+    {
+        return [
+            [
+                'http://user:userPass@example.org:3300/path?query=true#frag/ment',
+                [
+                    'http',
+                    '/path',
+                    'query=true',
+                    'frag/ment',
+                    'example.org',
+                    3300,
+                    'user',
+                    'userPass'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Test constructor
+     *
+     * This method validate the StringUri::_construct method
+     *
+     * @param string $uri        The constructor URI
+     * @param array  $components The uri components
+     *
+     * @dataProvider uriProvider
+     * @return       void
+     * @covers       Beotie\CoreBundle\Request\Uri\StringUri::__construct
+     */
+    public function testConstructor(string $uri, array $components) : void
+    {
+        $instance = new StringUri($uri);
+
+        $componentKeys = [
+            'scheme',
+            'path',
+            'query',
+            'fragment',
+            'host',
+            'port',
+            'user',
+            'pass'
+        ];
+        $components = array_combine($componentKeys, $components);
+
+        $reflectionClass = new \ReflectionClass(StringUri::class);
+        foreach ($componentKeys as $component) {
+            $property = $reflectionClass->getProperty($component);
+            $property->setAccessible(true);
+            $this->assertEquals($components[$component], $property->getValue($instance));
+        }
+
+        return;
+    }
+
+    /**
+     * Get test case
+     *
+     * This method return the current test case to be used as assertion provider
+     *
+     * @return TestCase
+     */
+    protected function getTestCase(): TestCase
+    {
+        return $this;
+    }
+}
